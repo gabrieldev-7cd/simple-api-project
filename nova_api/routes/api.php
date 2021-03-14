c808c69a1e57b2ba150880a62e29f1ddb75d8bd3
gabriel alexandre <gabriel_alexandre_correia@outlook.com>
2021-03-14 19:09:43 -0300

    migration de products com controller e model criados

--- nova_api/routes/api.php
+++ nova_api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', function(Request $request){
+    $response = new \Illuminate\Http\Response(json_encode(['msg' => 'Requested API' ]));
+    $response->header('Content-Type', 'application/json');
+
+    return $response;
+});
+
+// Route::get('/get_api', function(){
+//     return App\Models\Product::all();
+// });
+// Route::get('/gettest', [App\Http\Controllers\Api\Test]);
+
+Route::get('/getprodtest', [App\Http\Controllers\Api\TestController::class,'index'])->name('test');
+Route::get('/product', [App\Http\Controllers\Api\ProductController::class,'index'])->name('product');
