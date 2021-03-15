dda5eae1d29d5018e3a76363ce8c391dbc705bc8
gabriel alexandre <gabriel_alexandre_correia@outlook.com>
2021-03-15 01:39:59 -0300

    alteração da migration de produtos e fase inicial da api completa, endpoints iniciais instanciados

--- nova_api/app/Http/Controllers/Api/ProductController.php
+++ nova_api/app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class ProductController extends Controller
 {   
@@ -16,7 +17,44 @@ class ProductController extends Controller
     }
 
     public function index(){
-        $products = $this->product->all();
-        return response()->json($products);
+
+        return Product::orderBy('created_at', 'DESC')->get();
+        // $products = $this->product->all();
+        // return response()->json($products);
+        
+    }
+
+    public function store(Request $request){
+        $newProduct = new Product; 
+        
+        $newProduct->name = $request->product["name"];
+        $newProduct->price = $request->product["price"];
+        $newProduct->description = $request->product["description"];
+        $newProduct->slug = $request->product["slug"];
+        $newProduct->save();
+        return $newProduct;
+        
+    }
+
+    public function update(Request $request, $id){
+        $existingItem = Product::find($id);
+
+        if( $existingItem) {
+            $existingItem->completed = $request->product["completed"] ? true : false;
+            $existingItem->completed_at = $request->product["completed"] ? Carbon::now() : null;
+            $existingItem->save();
+            return $existingItem;
+        }
+        return "Item não encontrado!";
+    }
+    
+    public function destroy($id){
+        $existingItem = Product::find($id);
+        
+        if($existingItem){
+            $existingItem->delete();
+            return "Item deletado com sucesso!";
+        }
+        return "Item não encontrado!";
     }
 }
--- nova_api/routes/api.php
+++ nova_api/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api;
+use App\Http\Controllers\Api\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,14 +22,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/products', function(Request $request){
     $response = new \Illuminate\Http\Response(json_encode(['msg' => 'Requested API' ]));
     $response->header('Content-Type', 'application/json');
-
     return $response;
 });
 
-// Route::get('/get_api', function(){
-//     return App\Models\Product::all();
-// });
-// Route::get('/gettest', [App\Http\Controllers\Api\Test]);
+// Route::get('/get_api', function(){ return App\Models\Product::all(); });
 
-Route::get('/getprodtest', [App\Http\Controllers\Api\TestController::class,'index'])->name('test');
-Route::get('/product', [App\Http\Controllers\Api\ProductController::class,'index'])->name('product');
+// Route::get('/getprodtest', [App\Http\Controllers\Api\TestController::class,'index'])->name('test');
+// Route::get('/product', [App\Http\Controllers\Api\ProductController::class,'index'])->name('product');
+
+Route::get('/items', [ProductController::class, 'index']);
+Route::prefix('/item')->group(function () {
+    Route::post('/store', [ProductController::class, 'store']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
